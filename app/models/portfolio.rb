92ae171bfed93c55de857defb8fcaf3ad1f78ae6
Ieron <ieronman@gmail.com>
2018-10-18 12:02:43 -0700

    Implemented default values for images

--- app/models/portfolio.rb
+++ app/models/portfolio.rb
@@ -1,3 +1,17 @@
 class Portfolio < ApplicationRecord
-	validates_presence_of :title, :body, :thumb_image
+	validates_presence_of :title, :body, :main_image, :thumbnail_image
+
+	#def self.java
+	#	where(subtitle: 'Java')
+	#end
+
+	scope :java, -> { where(subtitle: 'Java')}
+	scope :ruby_on_rails, -> { where(subtitle: 'Ruby on Rails')}
+
+	after_initialize :set_defaults
+
+	def set_defaults
+		self.main_image ||= "https://place-hold.it/600x400"
+		self.thumbnail_image ||= "https://place-hold.it/350x200"
+	end
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -6,10 +6,17 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
+3.times do |topic|
+	Topic.create!(
+		title: "Topic #{topic}"
+	)
+end
+
 10.times do |blog|
 	Blog.create!(
 		title: "Blog Post #{blog}",
-		body: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
+		body: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
+		topic_id: Topic.last.id
 	)
 
 end
@@ -25,10 +32,20 @@ end
 
 puts "5 skills created"
 
-6.times do |portfolio_item|
+5.times do |portfolio_item|
 	Portfolio.create!(
 		title: "Portfolio title: #{portfolio_item}",
-		subtitle: "Somthing to give detail",
+		subtitle: "Ruby on Rails",
+		body: "Etiam et nisl iaculis odio bibendum ultricies non ac massa. Nam vitae iaculis massa. Phasellus volutpat ex sit amet mauris eleifend, at sodales lacus tempus. Aliquam aliquam quam sit amet enim commodo vestibulum. Suspendisse eget ultricies magna. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Etiam viverra, mauris dapibus porta interdum, ante metus porta eros, nec aliquam ipsum urna quis risus. Vivamus ornare fermentum mattis. Nullam a ullamcorper odio.",
+		main_image: "https://place-hold.it/600x400",
+		thumbnail_image: "https://place-hold.it/350x200"
+	)
+end
+
+1.times do |portfolio_item|
+	Portfolio.create!(
+		title: "Portfolio title: #{portfolio_item}",
+		subtitle: "Java",
 		body: "Etiam et nisl iaculis odio bibendum ultricies non ac massa. Nam vitae iaculis massa. Phasellus volutpat ex sit amet mauris eleifend, at sodales lacus tempus. Aliquam aliquam quam sit amet enim commodo vestibulum. Suspendisse eget ultricies magna. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Etiam viverra, mauris dapibus porta interdum, ante metus porta eros, nec aliquam ipsum urna quis risus. Vivamus ornare fermentum mattis. Nullam a ullamcorper odio.",
 		main_image: "https://place-hold.it/600x400",
 		thumbnail_image: "https://place-hold.it/350x200"

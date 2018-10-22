4204eaf07f7599557efda76022d7057a39c1843c
Ieron <ieronman@gmail.com>
2018-10-22 15:09:35 -0700

    Created virtual attrs for the user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,15 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  validates_presence_of :user_name
+
+  def first_name
+  	self.user_name.split.first
+  end
+
+  def last_name
+  	self.user_name.split.last
+  end
+
 end
